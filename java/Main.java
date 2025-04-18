58b59724c89eedf2377b20bd0bf90afee81bdace
Chanchal korsewada <106740763+rani1510@users.noreply.github.com>
2025-04-18 06:51:05 +0000

    updated

--- java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        StandardCalulator calc= new StandardCalulator();
-        int add=calc.add(2, 2);
-        System.out.println(add);
-        ScientificCalculator calc2=new ScientificCalculator();
-        System.out.println(calc2.cubeof(add));
-    }
-}
